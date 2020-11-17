505a647897192db81a403aa71d0abca8e0021eaf
Leonhard Kuboschek <leo@jacobs-alumni.de>
2020-11-17 15:17:27 +0100

    Reality update for before 2018

--- src/reality.ts
+++ src/reality.ts
@@ -165,6 +165,482 @@ const reality: AnyEvent[] = [
         }
     },
     {
+        type: EventType.PERSON_UPDATE,
+        personID: "ad",
+        personData: {
+            "name": "Alina Degtiarova"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "aa",
+        personData: {
+            "name": "Andres Alvarado"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "ah",
+        personData: {
+            "name": "Anna Habighorst"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "as",
+        personData: {
+            "name": "Anna-Lena Schindl"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "av",
+        personData: {
+            "name": "Anne Valtink"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "bw",
+        personData: {
+            "name": "Beenish Waris"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "dag",
+        personData: {
+            "name": "Daniel Alfonso Garcia"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "eg",
+        personData: {
+            "name": "Ewa Grzechnik"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "fm",
+        personData: {
+            "name": "Frederique Mittelstaedt"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "is",
+        personData: {
+            "name": "Irina Shames"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "im",
+        personData: {
+            "name": "Ivan Mandadzhiev"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "jg",
+        personData: {
+            "name": "Julia Gyorffy"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "km",
+        personData: {
+            "name": "Konstantin Mihov"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "ll",
+        personData: {
+            "name": "Lida-Maria Lottko"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "ml",
+        personData: {
+            "name": "Max Lohmann"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "mw",
+        personData: {
+            "name": "Melanie Wilneder"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "md",
+        personData: {
+            "name": "Mohsin Dar"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "pp",
+        personData: {
+            "name": "Petar Perkovic"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "pd",
+        personData: {
+            "name": "Peter Dabrowski"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "ph",
+        personData: {
+            "name": "Philipp Herzberg"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "rb",
+        personData: {
+            "name": "Răzvan Barabaș"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "re",
+        personData: {
+            "name": "Robert Eckhoff"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "rp",
+        personData: {
+            "name": "Raluca Puie"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "sa",
+        personData: {
+            "name": "Stefan Anca"
+        }
+    },
+    {
+        type: EventType.PERSON_UPDATE,
+        personID: "st",
+        personData: {
+            "name": "Suna Turhan"
+        }
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2004-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "rp",
+        role: BoardRole.President
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2004-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2006-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2006-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "rp"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2007-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "aa",
+        role: BoardRole.President
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "mw",
+        role: BoardRole.Secretary
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "re",
+         BoardRole.AssessorSpecialProjects
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2007-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2009-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2009-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "aa"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "mw"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2010-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "eg",
+        role: BoardRole.Treasurer
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "is",
+        role: BoardRole.VicePresident
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "pp",
+        role: BoardRole.President
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "sa",
+        role: BoardRole.Secretary
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2010-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "is"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2011-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "jg",
+        role: BoardRole.AssessorSpecialProjects
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "km",
+        role: BoardRole.AssessorSpecialProjects
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "pd",
+        role: BoardRole.VicePresident
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2011-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2012-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2012-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "eg"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "pp"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "pd"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2013-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "dt",
+        role: BoardRole.VicePresident
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "pd",
+        role: BoardRole.President
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "st",
+        role: BoardRole.Treasurer
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2013-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "dt"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "jg"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "km"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "re"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2014-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "ad",
+        role: BoardRole.AssessorPublicRelations
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "dag",
+        role: BoardRole.VicePresident
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2014-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "st"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2015-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "ah",
+        role: BoardRole.Treasurer
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "ph",
+        role: BoardRole.AssessorMemberManagement
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2015-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "ad"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "ah"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "dag"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "pd"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "sa"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2016-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "as",
+        role: BoardRole.AssessorNetworkDevelopment
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "av",
+        role: BoardRole.President
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "ei",
+        role: BoardRole.AssessorSpecialProjects
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "fm",
+        role: BoardRole.AssessorInformationTechnology
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "ll",
+        role: BoardRole.AssessorPublicRelations
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "ml",
+        role: BoardRole.AssessorEventManagement
+    },
+    {
+        type: EventType.ELECTED,
+        personID: "rb",
+        role: BoardRole.Secretary
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2016-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "ph"
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2017-01-01T00:00:00.000Z")
+    },
+    {
+        type: EventType.TIME,
+        now: new Date("2017-12-31T23:00:00.000Z")
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "fm"
+    },
+    {
+        type: EventType.VOTED_OUT,
+        personID: "ll"
+    },
+    {
         type: EventType.TIME,
         now: new Date('2018-01-15') // TODO More specific
     },
